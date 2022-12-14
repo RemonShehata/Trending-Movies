8b378ceea07762e2ccce7ab44291b6764274970f
Remon Shehata <remongerges94@gmail.com>
2022-12-14 17:01:27 +0200

    get all movies sync from db

--- app/src/main/java/com/example/trendingmovies/database/TrendingMoviesDao.kt
+++ app/src/main/java/com/example/trendingmovies/database/TrendingMoviesDao.kt
@@ -3,6 +3,7 @@ package com.example.trendingmovies.database
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface TrendingMoviesDao {
@@ -12,4 +13,7 @@ interface TrendingMoviesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovies(moviesEntity: List<TrendingMoviesEntity>) //TODO: Do we need long return here?
+
+    @Query("SELECT * FROM TrendingMoviesEntity")
+    suspend fun getAllMoviesSync(): List<TrendingMoviesEntity>
 }
