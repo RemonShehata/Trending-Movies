5746d6a8a67b17b399c6add2913e6b59f146232b
Remon Shehata <remongerges94@gmail.com>
2022-12-14 14:30:04 +0200

    add trending movies entity add type converters

--- app/src/main/java/com/example/trendingmovies/database/TrendingMoviesEntity.kt
+++ app/src/main/java/com/example/trendingmovies/database/TrendingMoviesEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class TrendingMoviesEntity(
-    @PrimaryKey(autoGenerate = false) val id: Int,
+    @PrimaryKey(autoGenerate = false) @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "poster_path") val posterPath: String?,
     @ColumnInfo(name = "adult") val adult: Boolean,
     @ColumnInfo(name = "overview")
@@ -14,7 +14,7 @@ data class TrendingMoviesEntity(
     @ColumnInfo(name = "release_date")
     val releaseDate: String,
     @ColumnInfo(name = "genre_ids")
-    val genreIds: IntArray, // instead of Array<Int> https://kotlinlang.org/docs/arrays.html#primitive-type-arrays
+    val genreIds: List<Int>, // had an issue with IntArray type converter, so using List<Int> for now
     @ColumnInfo(name = "original_title")
     val originalTitle: String,
     @ColumnInfo(name = "original_language")
@@ -32,46 +32,4 @@ data class TrendingMoviesEntity(
     @ColumnInfo(name = "vote_average")
     val voteAverage: Double,
 
-    ) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as TrendingMoviesEntity
-
-        if (id != other.id) return false
-        if (posterPath != other.posterPath) return false
-        if (adult != other.adult) return false
-        if (overview != other.overview) return false
-        if (releaseDate != other.releaseDate) return false
-        if (!genreIds.contentEquals(other.genreIds)) return false
-        if (originalTitle != other.originalTitle) return false
-        if (originalLanguage != other.originalLanguage) return false
-        if (title != other.title) return false
-        if (backdropPath != other.backdropPath) return false
-        if (popularity != other.popularity) return false
-        if (voteCount != other.voteCount) return false
-        if (video != other.video) return false
-        if (voteAverage != other.voteAverage) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id
-        result = 31 * result + (posterPath?.hashCode() ?: 0)
-        result = 31 * result + adult.hashCode()
-        result = 31 * result + overview.hashCode()
-        result = 31 * result + releaseDate.hashCode()
-        result = 31 * result + genreIds.contentHashCode()
-        result = 31 * result + originalTitle.hashCode()
-        result = 31 * result + originalLanguage.hashCode()
-        result = 31 * result + title.hashCode()
-        result = 31 * result + (backdropPath?.hashCode() ?: 0)
-        result = 31 * result + popularity.hashCode()
-        result = 31 * result + voteCount
-        result = 31 * result + video.hashCode()
-        result = 31 * result + voteAverage.hashCode()
-        return result
-    }
-}
+    )
---
+++ app/src/main/java/com/example/trendingmovies/database/TypeConverters.kt
@@ -0,0 +1,80 @@
+package com.example.trendingmovies.database
+
+import androidx.room.TypeConverter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+private val moshi: Moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+//object GenreIdsConverter {
+//    private val genreIdsType =
+//        Types.newParameterizedType(IntArray::class.java, TrendingMoviesEntity::class.java)
+//    private val genreIdsAdapter =
+//        moshi.adapter<IntArray>(genreIdsType)
+//
+//    @TypeConverter
+//    @JvmStatic
+//    fun genreIdsToString(genreIds: IntArray): String =
+//        genreIdsAdapter.toJson(genreIds)
+//
+//    @TypeConverter
+//    @JvmStatic
+//    fun stringToGenreIds(genreIds: String): IntArray? =
+//        genreIdsAdapter.fromJson(genreIds)
+//}
+
+object GenreIdsConverter {
+    private val genreIdsType =
+        Types.newParameterizedType(List::class.java, Int::class.javaObjectType)
+    private val genreIdsAdapter =
+        moshi.adapter<List<Int>>(genreIdsType)
+
+    @TypeConverter
+    @JvmStatic
+    fun genreIdsToString(genreIds: List<Int>): String =
+        genreIdsAdapter.toJson(genreIds)
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToGenreIds(genreIds: String): List<Int> =
+        genreIdsAdapter.fromJson(genreIds).orEmpty()
+}
+
+object TrendingMoviesConverter {
+    private val GenreType =
+        Types.newParameterizedType(List::class.java, TrendingMoviesEntity::class.java)
+    private val trendingMoviesAdapter =
+        moshi.adapter<List<TrendingMoviesEntity>>(GenreType)
+
+    @TypeConverter
+    @JvmStatic
+    fun trendingMoviesToString(trendingMoviesEntities: List<TrendingMoviesEntity>): String =
+        trendingMoviesAdapter.toJson(trendingMoviesEntities)
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToTrendingMovies(trendingMoviesEntities: String): List<TrendingMoviesEntity> =
+        trendingMoviesAdapter.fromJson(trendingMoviesEntities).orEmpty()
+}
+
+object GenreConverter {
+    private val trendingMoviesType =
+        Types.newParameterizedType(List::class.java, TrendingMoviesEntity::class.java)
+    private val trendingMoviesAdapter =
+        moshi.adapter<List<Genre>>(trendingMoviesType)
+
+    @TypeConverter
+    @JvmStatic
+    fun genresToString(genres: List<Genre>): String =
+        trendingMoviesAdapter.toJson(genres)
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToGenres(genres: String): List<Genre> =
+        trendingMoviesAdapter.fromJson(genres).orEmpty()
+
+
+}
