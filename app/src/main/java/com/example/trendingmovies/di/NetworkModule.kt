17195d3b07b9e0b57922d74d911f7cec149ad66a
Remon Shehata <remongerges94@gmail.com>
2022-12-14 15:03:38 +0200

    get MoviesApi to use hilt DI

---
+++ app/src/main/java/com/example/trendingmovies/di/NetworkModule.kt
@@ -0,0 +1,104 @@
+package com.example.trendingmovies.di
+
+import com.example.trendingmovies.BuildConfig
+import com.example.trendingmovies.network.MoviesApi
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideMoviesApi(retrofit: Retrofit): MoviesApi{
+        return retrofit.create(MoviesApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        moshiConverterFactory: MoshiConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(TMDB_BASE_URL)
+            .addConverterFactory(
+                moshiConverterFactory
+            )
+
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshiConverterFactory(moshi: Moshi): MoshiConverterFactory {
+        return MoshiConverterFactory.create(moshi).withNullSerialization()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        apiKeyInterceptor: Interceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+
+            builder.addInterceptor(httpLoggingInterceptor)
+        }
+
+        builder.addInterceptor(apiKeyInterceptor)
+        return builder.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggerInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiKeyInterceptor(): Interceptor {
+        val interceptor = Interceptor { chain ->
+            var request: Request = chain.request()
+            val url: HttpUrl =
+                request.url.newBuilder()
+                    .addQueryParameter(API_KEY_PARAM, BuildConfig.TMDB_API_KEY)
+                    .build()
+
+            request = request.newBuilder().url(url).build()
+            chain.proceed(request)
+        }
+
+        return interceptor
+    }
+
+    companion object {
+        private const val CONNECT_TIMEOUT_SECONDS = 15L
+        private const val READ_TIMEOUT_SECONDS = 15L
+        private const val WRITE_TIMEOUT_SECONDS = 15L
+        private const val API_KEY_PARAM = "api_key"
+        private const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+    }
+}
--- app/src/main/java/com/example/trendingmovies/network/MoviesApi.kt
+++ app/src/main/java/com/example/trendingmovies/network/MoviesApi.kt
@@ -1,18 +1,8 @@
 package com.example.trendingmovies.network
 
-import com.example.trendingmovies.BuildConfig
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.*
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
-import java.util.concurrent.TimeUnit
 
-
-const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
 
 interface MoviesApi {
 
@@ -24,51 +14,4 @@ interface MoviesApi {
 
     @GET("configuration")
     suspend fun getConfiguration(): Configuration
-
-    companion object {
-        fun create(): MoviesApi {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(TMDB_BASE_URL)
-                .addConverterFactory(
-                    MoshiConverterFactory.create(createMoshi()).withNullSerialization()
-                )
-
-                .client(createOkHttpClient())
-                .build()
-            return retrofit.create(MoviesApi::class.java)
-        }
-
-        private fun createOkHttpClient(): OkHttpClient {
-            val builder = OkHttpClient.Builder()
-                .connectTimeout(15, TimeUnit.SECONDS)
-                .readTimeout(15, TimeUnit.SECONDS)
-                .writeTimeout(15, TimeUnit.SECONDS)
-
-            if (BuildConfig.DEBUG) {
-                val httpLoggingInterceptor = HttpLoggingInterceptor()
-                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-                builder.addInterceptor(httpLoggingInterceptor)
-            }
-
-            builder.addInterceptor(apiKeyInterceptor)
-            return builder.build()
-        }
-
-        private fun createMoshi(): Moshi {
-            return Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-        }
-
-        private val apiKeyInterceptor = Interceptor { chain ->
-            var request: Request = chain.request()
-            val url: HttpUrl =
-                request.url.newBuilder()
-                    .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
-                    .build()
-
-            request = request.newBuilder().url(url).build()
-            chain.proceed(request)
-        }
-    }
 }
