fe00b1bf0425ad3c38afffda9282cfed4642606a
Remon Shehata <remongerges94@gmail.com>
2022-12-15 18:24:50 +0200

    implement details dao insert and query

--- app/src/main/java/com/example/trendingmovies/database/MovieDetailsDao.kt
+++ app/src/main/java/com/example/trendingmovies/database/MovieDetailsDao.kt
@@ -1,7 +1,15 @@
 package com.example.trendingmovies.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface MovieDetailsDao {
+
+    @Insert
+    fun insertMovieDetails(movieDetailsEntity: MovieDetailsEntity): Long
+
+    @Query("SELECT * FROM MovieDetailsEntity WHERE id = :movieId LIMIT 1")
+    fun getMovieById(movieId: String): MovieDetailsEntity
 }
