5c419eef46fb3c795dc49b5ebab2bdd2fcebde7a
Remon Shehata <remongerges94@gmail.com>
2022-12-15 22:30:17 +0200

    add query for web api to get the data based on the page

--- app/src/main/java/com/example/trendingmovies/network/MoviesApi.kt
+++ app/src/main/java/com/example/trendingmovies/network/MoviesApi.kt
@@ -2,12 +2,13 @@ package com.example.trendingmovies.network
 
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 
 interface MoviesApi {
 
     @GET("discover/movie")
-    suspend fun getTrendingMovies(): MoviesResponse
+    suspend fun getTrendingMovies(@Query("page") page: Int = 1): MoviesResponse
 
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(@Path("movie_id") movieId: String): MovieDetailsResponse
