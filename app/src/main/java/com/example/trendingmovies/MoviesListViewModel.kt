44407c82fd3075d30ec421887b744c0675b0e744
Remon Shehata <remongerges94@gmail.com>
2022-12-15 13:55:28 +0200

    get the full data class and post to livedata

--- app/src/main/java/com/example/trendingmovies/MoviesListViewModel.kt
+++ app/src/main/java/com/example/trendingmovies/MoviesListViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.trendingmovies
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.trendingmovies.utils.toTrendingMovieDtoList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
@@ -14,9 +17,16 @@ class MoviesListViewModel @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
+    private val moviesMutableLiveData: MutableLiveData<List<TrendingMoviesDto>> = MutableLiveData()
+    val moviesLiveData: LiveData<List<TrendingMoviesDto>> = moviesMutableLiveData
+
     fun getAllMovies() {
         viewModelScope.launch(ioDispatcher) {
-            val result = moviesListRepo.getAllMoviesSync()
+            val moviesResult = moviesListRepo.getAllMoviesSync()
+            val configurationResult = configurationRepo.getConfiguration()
+
+            val movies = configurationResult toTrendingMovieDtoList moviesResult
+            moviesMutableLiveData.postValue(movies)
         }
     }
 }
