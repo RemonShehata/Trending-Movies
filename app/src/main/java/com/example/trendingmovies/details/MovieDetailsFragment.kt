22f3d8a6ed7735c05077f15ae0c5b811df4c3c99
Remon Shehata <remongerges94@gmail.com>
2022-12-15 23:09:42 +0200

    fix a bug in displaying status

--- app/src/main/java/com/example/trendingmovies/details/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/trendingmovies/details/MovieDetailsFragment.kt
@@ -56,9 +56,12 @@ class MovieDetailsFragment : Fragment() {
                 binding.genre2TextView.isVisible = movie.genres.size > 1
                 binding.genre3TextView.isVisible = movie.genres.size > 2
 
-                binding.genre1TextView.text = movie.genres[0]
-                binding.genre2TextView.text = movie.genres[1]
-                binding.genre3TextView.text = movie.genres[2]
+                when (movie.genres.size) {
+                    1 -> binding.genre1TextView.text = movie.genres[0]
+                    2 -> binding.genre2TextView.text = movie.genres[1]
+                    3 -> binding.genre3TextView.text = movie.genres[2]
+                }
+
                 binding.releaseDateTextView.text = movie.releaseDate
                 binding.votCountTextView.text = movie.voteCount
 
