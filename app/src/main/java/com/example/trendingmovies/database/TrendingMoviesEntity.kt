dce7974c17b40088f329e8c9d1b8c9d9ade78193
Remon Shehata <remongerges94@gmail.com>
2022-12-16 22:03:57 +0200

    format TrendingMovieEntity columns

--- app/src/main/java/com/example/trendingmovies/database/TrendingMoviesEntity.kt
+++ app/src/main/java/com/example/trendingmovies/database/TrendingMoviesEntity.kt
@@ -6,31 +6,20 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class TrendingMoviesEntity(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "auto_id") val autoId: Int = 0,
-    @ColumnInfo(name = "id") val id: Int,
+    @PrimaryKey(autoGenerate = false) @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "poster_path") val posterPath: String?,
     @ColumnInfo(name = "adult") val adult: Boolean,
-    @ColumnInfo(name = "overview")
-    val overview: String,
-    @ColumnInfo(name = "release_date")
-    val releaseDate: String,
-    @ColumnInfo(name = "genre_ids")
-    val genreIds: List<Int>, // had an issue with IntArray type converter, so using List<Int> for now
-    @ColumnInfo(name = "original_title")
-    val originalTitle: String,
-    @ColumnInfo(name = "original_language")
-    val originalLanguage: String,
-    @ColumnInfo(name = "title")
-    val title: String,
-    @ColumnInfo(name = "backdrop_path")
-    val backdropPath: String?,
-    @ColumnInfo(name = "popularity")
-    val popularity: Double,
-    @ColumnInfo(name = "vote_count")
-    val voteCount: Int,
-    @ColumnInfo(name = "video")
-    val video: Boolean,
-    @ColumnInfo(name = "vote_average")
-    val voteAverage: Double,
-
-    )
+    @ColumnInfo(name = "overview") val overview: String,
+    @ColumnInfo(name = "release_date") val releaseDate: String,
+    // had an issue with IntArray type converter, so using List<Int> for now
+    @ColumnInfo(name = "genre_ids") val genreIds: List<Int>,
+    @ColumnInfo(name = "original_title") val originalTitle: String,
+    @ColumnInfo(name = "original_language") val originalLanguage: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "backdrop_path") val backdropPath: String?,
+    @ColumnInfo(name = "popularity") val popularity: Double,
+    @ColumnInfo(name = "vote_count") val voteCount: Int,
+    @ColumnInfo(name = "video") val video: Boolean,
+    @ColumnInfo(name = "vote_average") val voteAverage: Double,
+    @ColumnInfo(name = "timestamp_millis") val timestampMillis: Long
+)
