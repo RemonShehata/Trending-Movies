f1364c93b3957f1e5ae74a1d538b8d6b57145451
Remon Shehata <remongerges94@gmail.com>
2023-01-26 19:56:51 +0200

    add back arrow and rename the toolbar label

--- app/src/main/java/com/example/trendingmovies/base/MainActivity.kt
+++ app/src/main/java/com/example/trendingmovies/base/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.trendingmovies.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.trendingmovies.R
 import com.example.trendingmovies.core.source.local.MoviesDatabase
 import com.example.trendingmovies.databinding.ActivityMainBinding
 import com.example.trendingmovies.core.source.remote.MoviesApi
@@ -19,10 +24,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 
