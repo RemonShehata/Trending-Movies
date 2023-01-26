aeb794331f92f366b4cd83de4057a94be989d819
Remon Shehata <remongerges94@gmail.com>
2023-01-26 15:20:52 +0200

    init viewmodel test

--- app/src/test/java/com/example/trendingmovies/features/movies_list/TrendingMoviesViewModelTest.kt
+++ app/src/test/java/com/example/trendingmovies/features/movies_list/TrendingMoviesViewModelTest.kt
@@ -1,21 +1,33 @@
 package com.example.trendingmovies.features.movies_list
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.trendingmovies.State
+import com.example.trendingmovies.core.models.TrendingMoviesDto
+import com.example.trendingmovies.core.source.local.models.ConfigurationEntity
 import com.example.trendingmovies.core.source.repos.ConfigurationRepo
 import com.example.trendingmovies.core.source.repos.TrendingMoviesRepo
 import com.example.trendingmovies.util.TestCoroutineRule
+import com.example.trendingmovies.util.getOrAwaitValue
+import com.example.trendingmovies.utils.NetworkState
 import com.example.trendingmovies.utils.NetworkStateMonitor
-import io.mockk.MockKAnnotations
+import io.mockk.*
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.unmockkAll
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TestRule
 
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class TrendingMoviesViewModelTest {
     @get:Rule
     val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
@@ -34,21 +46,87 @@ internal class TrendingMoviesViewModelTest {
     @MockK
     private lateinit var networkStateMonitor: NetworkStateMonitor
 
-    private val testDispatcher: CoroutineDispatcher = UnconfinedTestDispatcher()
+    private val testDispatcher: CoroutineDispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        trendingMoviesViewModel = TrendingMoviesViewModel(
-            trendingMoviesRepo, configurationRepo,
-            networkStateMonitor, testDispatcher
-        )
-        // https://stackoverflow.com/questions/44382540/mocking-extension-function-in-kotlin
-//        mockkStatic("com.example.newsapp.utils.ExtentionsUtilKt")
+//        trendingMoviesViewModel = TrendingMoviesViewModel(
+//            trendingMoviesRepo, configurationRepo,
+//            networkStateMonitor, testDispatcher
+//        )
+        // https://stackoverflow.com/questions/36787449/how-to-mock-method-e-in-log
+//        mockkStatic(Log::class)
+//        every { Log.v(any(), any()) } returns 0
+//        every { Log.d(any(), any()) } returns 0
+//        every { Log.i(any(), any()) } returns 0
+//        every { Log.e(any(), any()) } returns 0
     }
 
     @After
     fun tearDown() {
         unmockkAll()
+    }
+
+    @Test
+    fun `test viewmodel init`() = testCoroutineRule.runBlockingTest {
+        // GIVEN
+        val networkMutableStateFlow = MutableStateFlow(NetworkState.Connected)
+        every { trendingMoviesRepo.getAllMoviesFlow() } returns flowOf(emptyList())
+        coEvery { trendingMoviesRepo.getMoviesForPage() } returns Unit
+        coEvery { configurationRepo.getConfiguration() } returns getConfigurationEntity()
+        networkMutableStateFlow.emit(NetworkState.Connected)
+        every { networkStateMonitor.networkStateFlow } returns networkMutableStateFlow
+
+        // WHEN
+        testCoroutineRule.pauseDispatcher()
+        trendingMoviesViewModel = TrendingMoviesViewModel(
+            trendingMoviesRepo, configurationRepo,
+            networkStateMonitor, Dispatchers.Main
+        )
+
+        // THEN
+//        val observer = mockk<Observer<State<List<TrendingMoviesDto>>> { every { onChanged(any()) } just Runs }
+//        val x = mockk<Observer<State<List<TrendingMoviesDto>>>>()
+//        every { x.onChanged(any()) } just Runs
+
+
+        val result = trendingMoviesViewModel.moviesLiveData.getOrAwaitValue()
+        assert(result is State.Loading)
+        testCoroutineRule.resumeDispatcher()
+        val result2 = trendingMoviesViewModel.moviesLiveData.getOrAwaitValue()
+        assert(result2 is State.Success)
+
+    }
+
+    @Test
+    fun `given page table is empty when getMoviesForPage is called, `() = runTest {
+
+        every { trendingMoviesRepo.getAllMoviesFlow() } returns flowOf(emptyList())
+        coEvery { configurationRepo.getConfiguration() } returns getConfigurationEntity()
+
+        trendingMoviesViewModel = TrendingMoviesViewModel(
+            trendingMoviesRepo, configurationRepo,
+            networkStateMonitor, testDispatcher
+        )
+
+        trendingMoviesViewModel.getNextPageData()
+
+
+        val result = trendingMoviesViewModel.moviesLiveData.getOrAwaitValue()
+        assert(result is State.Loading)
+    }
+
+    private fun getConfigurationEntity(): ConfigurationEntity {
+        return ConfigurationEntity(
+            baseUrl = "",
+            secureBaseUrl = "",
+            backdropSizes = emptyList(),
+            logoSizes = emptyList(),
+            posterSizes = emptyList(),
+            profileSizes = emptyList(),
+            stillSizes = emptyList(),
+            changeKeys = emptyList()
+        )
     }
 }
